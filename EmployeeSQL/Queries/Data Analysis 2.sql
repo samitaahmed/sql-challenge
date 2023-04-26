94522fcf64db2770c4c3537aaaf985be3703b934
samitaahmed <samitaahmed@gmail.com>
2023-04-26 18:34:07 -0400

    query 5

--- EmployeeSQL/Queries/Data Analysis 2.sql
+++ EmployeeSQL/Queries/Data Analysis 2.sql
@@ -1,2 +1,26 @@
+-- Data Analysis 2:
+
 SELECT first_name, last_name, hire_date from employees 
 where EXTRACT(YEAR FROM hire_date) = 1986
+
+
+-- Data Analysis 3:
+
+SELECT dept_manager.dept_no, dept_manager.emp_no, dept_name, last_name, first_name 
+FROM dept_manager
+LEFT JOIN departments on dept_manager.dept_no = departments.dept_no
+LEFT JOIN employees ON dept_manager.emp_no = employees.emp_no
+ 
+
+-- Data Analysis 4: 
+
+SELECT dept_emp.dept_no, dept_emp.emp_no, last_name, first_name, dept_name
+FROM dept_emp
+LEFT JOIN employees ON dept_emp.emp_no = employees.emp_no
+LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no
+
+
+-- Data Analysis 5:
+
+Select first_name, last_name, sex From employees
+Where first_name = 'Hercules' AND last_name like 'B%'
