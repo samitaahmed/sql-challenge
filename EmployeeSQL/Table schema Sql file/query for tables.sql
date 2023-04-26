5bae2df824f018f7136a821d2e6c38ac884044a1
samitaahmed <samitaahmed@gmail.com>
2023-04-26 13:09:20 -0400

    Update query for tables.sql

--- EmployeeSQL/Table schema Sql file/query for tables.sql
+++ EmployeeSQL/Table schema Sql file/query for tables.sql
@@ -1,17 +1,26 @@
+DROP TABLE IF  EXISTS departments;
+DROP TABLE IF  EXISTS dept_emp;
+DROP TABLE IF  EXISTS dept_manager;
+DROP TABLE IF  EXISTS employees;
+DROP TABLE IF  EXISTS titles;
+DROP TABLE IF  EXISTS salaries;
+
 CREATE Table departments(
 	dept_no Varchar (10) Primary Key Not Null,
 	dept_name Varchar(30));
 	
 CREATE Table dept_emp(
-emp_no int Primary Key Not Null,
+emp_no Varchar(10),
 dept_no Varchar(10));
+ALTER TABLE dept_emp 
+ADD PRIMARY KEY (emp_no, dept_no);
 
 CREATE TABLE dept_manager(
 dept_no	Varchar(10),
-emp_no int Primary Key Not Null);
+emp_no Varchar(10) Primary Key Not Null);
 
 CREATE TABLE employees(
-emp_no int Primary Key Not Null,
+emp_no Varchar(10) Primary Key Not Null,
 emp_title_id Varchar(10),
 birth_date date,
 first_name Varchar (30),
@@ -20,7 +29,7 @@ sex Varchar(1),
 hire_date date);
 
 CREATE TABLE salaries(
-emp_no int Primary Key Not Null,
+emp_no Varchar(10) Primary Key Not Null,
 salary int);
 
 CREATE TABLE titles(
