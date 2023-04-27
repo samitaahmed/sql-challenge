8f2353e1ebcb3bbf5dc9a3bc3a913c98a247704c
samitaahmed <samitaahmed@gmail.com>
2023-04-26 21:39:46 -0400

    query 8

--- EmployeeSQL/Queries/SQL Queries.sql
+++ EmployeeSQL/Queries/SQL Queries.sql
@@ -7,13 +7,13 @@ select e.emp_no AS employee_number
 , s.salary
 from employees e 
 LEFT JOIN salaries s  
-ON e.emp_no = s.emp_no
+ON e.emp_no = s.emp_no;
 
 
 -- Query 2:
 
 SELECT first_name, last_name, hire_date from employees 
-where EXTRACT(YEAR FROM hire_date) = 1986
+where EXTRACT(YEAR FROM hire_date) = 1986;
 
 
 -- Query 3:
@@ -21,7 +21,7 @@ where EXTRACT(YEAR FROM hire_date) = 1986
 SELECT dept_manager.dept_no, dept_manager.emp_no, dept_name, last_name, first_name 
 FROM dept_manager
 LEFT JOIN departments on dept_manager.dept_no = departments.dept_no
-LEFT JOIN employees ON dept_manager.emp_no = employees.emp_no
+LEFT JOIN employees ON dept_manager.emp_no = employees.emp_no;
  
 
 -- Query 4: 
@@ -29,20 +29,20 @@ LEFT JOIN employees ON dept_manager.emp_no = employees.emp_no
 SELECT dept_emp.dept_no, dept_emp.emp_no, last_name, first_name, dept_name
 FROM dept_emp
 LEFT JOIN employees ON dept_emp.emp_no = employees.emp_no
-LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no
+LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no;
 
 
 -- Query 5:
 
 Select first_name, last_name, sex From employees
-Where first_name = 'Hercules' AND last_name like 'B%'
+Where first_name = 'Hercules' AND last_name like 'B%';
 
 -- Query 6: 
 
 SELECT departments.dept_name, dept_emp.emp_no, last_name, first_name FROM departments
 LEFT JOIN dept_emp ON departments.dept_no = dept_emp.dept_no
 LEFT JOIN employees ON dept_emp.emp_no = employees. emp_no
-WHERE departments.dept_name = 'Sales'
+WHERE departments.dept_name = 'Sales';
 
 
 -- Query 7:
@@ -50,10 +50,14 @@ WHERE departments.dept_name = 'Sales'
 SELECT departments.dept_name, dept_emp.emp_no, last_name, first_name FROM departments
 LEFT JOIN dept_emp ON departments.dept_no = dept_emp.dept_no
 LEFT JOIN employees ON dept_emp.emp_no = employees. emp_no
-WHERE departments.dept_name in ('Sales', 'Development')
+WHERE departments.dept_name in ('Sales', 'Development');
 
 -- Query 8:
 
+SELECT last_name, count(last_name) As frequency
+From employees
+group by last_name
+Order by count(last_name) desc
 
 
 
